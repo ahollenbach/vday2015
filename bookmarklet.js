3ed2221de3b3f4a14397a9d6121826fdc2fecc00
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2015-02-14 17:17:36 -0500

    Switched to gifs for faster rendering
    Allows for more hearts

--- bookmarklet.js
+++ bookmarklet.js
@@ -18,9 +18,8 @@ javascript: (function () {
           + "html,body {background-color:#280206 !important}"
           + "p,a { background-color: #4C060B !important;color: white !important}"
           + "h1,h2,h3,h4,h5,h6 { background-color: #DD466F !important;color: #280206 !important}"
-          + "@-webkit-keyframes resize { 0% { font-size: 0px; color: black; } 100% { font-size: 100px; color: red; } }"
-          + ".heart { background: none !important; }"
-          + "span.heart {position: absolute;height: 200px;width: 200px;text-align: center;color: red;-webkit-animation-name: resize;-webkit-animation-duration: 0.6s;-webkit-animation-iteration-count: infinite;-webkit-animation-direction: alternate;-webkit-animation-timing-function: ease-in-out;}";
+          + ".hearts { position: absolute; top:0;left:0;}"
+          + ".heart {background: none !important;position: absolute;height: 200px;width: 200px;z-index:3000;}";
   var style = document.createElement('style');
 
   style.type = 'text/css';
@@ -32,42 +31,31 @@ javascript: (function () {
   heartDiv.className = 'hearts';
   document.body.appendChild(heartDiv);
 
-  // Heart class represents a beating heart. It is implemented using a span
-  // and a heart character. Coloring and resizing the span to create the
-  // effect
   var Heart = function(x, y) {
-    this.initalize(x, y);
-  };
-  Heart.prototype = {
-    initalize: function(x, y) {
-      // create an element
-      this.element = document.createElement("span");
-      this.element.className = "heart";
-      this.element.style.top = y + "px";
-      this.element.style.left = x + "px";
-
-      // randomly different heart style
-      this.element.innerText = (Math.random() > 0.5) ? "\u2661" : "\u2665";
-    }
+    this.element = document.createElement("img");
+    this.element.className = "heart";
+    this.element.style.top = y + "px";
+    this.element.style.left = x + "px";
+    this.element.src = 'https://terry1954.files.wordpress.com/2013/06/beating-heart.gif';
   };
   function randInt(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
   }
   function makeHeart(x,y) {
     x = typeof x !== 'undefined' ? x : randInt(10,window.innerWidth-10);
-    y = typeof y !== 'undefined' ? x : randInt(10,window.innerHeight-10);
+    y = typeof y !== 'undefined' ? y : randInt(10,window.innerHeight-10);
     var heart = new Heart(x, y);
-    if(heartDiv.children.length >= 10) {
+    if(heartDiv.children.length >= 40) {
         heartDiv.removeChild(heartDiv.children[0]);
     }
     heartDiv.appendChild(heart.element);
   }
 
   // register on click to create a new heart
-  // document.onclick = function(event) {
-  //   makeHeart(event.clientX, event.clientY)
-  // };
-  for(var i=0;i<7;i++) {
+  document.onclick = function(event) {
+    makeHeart(event.pageX-150, event.pageY-150);
+  };
+  for(var i=0;i<14;i++) {
     makeHeart();
   }
 }());
